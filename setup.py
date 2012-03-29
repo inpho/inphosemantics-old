8d1c29b29880b32d1e4e9b6d9216e95b674c4db8
Jaimie Murdock <jaimie.murdock@gmail.com>
2012-03-29 15:47:53 -0400

    adding package infrastructure

---
+++ setup.py
@@ -0,0 +1,30 @@
+# chardet's setup.py
+from distutils.core import setup
+import setuptools
+setup(
+    name = "inphosemantics",
+    version = "0.1",
+    description = "Indiana Philosophy Ontology Project data mining tools",
+    author = "The Indiana Philosophy Ontology (InPhO) Project",
+    author_email = "inpho@indiana.edu",
+    url = "http://inpho.cogs.indiana.edu/",
+    download_url = "http://www.github.com/inpho/inphosemantics",
+    keywords = ["taxonomy", "ontology"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    install_requires=[
+        "numpy>=1.4.0,<=1.4.99",
+        "nltk>=2.0.0"
+    ],
+)
