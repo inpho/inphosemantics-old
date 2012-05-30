5b9c35142d2ac1d1df7ba1973f2cf675b83410cb
Robert Rose <rrose1@indiana.edu>
2012-05-30 13:23:44 -0400

    Added utilities in util.py for generating nwb and word2word files from a symmetric matrix and row/col labels.

---
+++ inphosemantics/util.py
@@ -0,0 +1,42 @@
+
+def gen_nwb(matrix, labels):
+
+    out = ''
+
+    out += '*Nodes\n'
+    out += 'id*int label*string\n'
+    
+    for i in xrange(1, len(labels)+1):
+        out += '{0} "{1}"\n'.format(i, labels[i-1])
+
+    out += '*UndirectedEdges\n'
+    out += 'source*int target*int weight*float\n'
+
+    for i in xrange(matrix.shape[0]):
+        for j in xrange(i+1, matrix.shape[1]):
+
+            out += '{0} {1} {2}\n'.format(i+1, j+1, matrix[i,j])
+
+
+    return out
+
+
+
+
+
+def gen_word2word(matrix, labels):
+
+    out = ''
+
+    labels = ['"' + label + '"' for label in labels]
+
+    out += ' , ' + ', '.join(labels) + '\n'
+
+    for i in xrange(matrix.shape[0]):
+        
+        values = [str(value) for value in matrix[i,:]]
+        
+        out += labels[i] + ', ' + ', '.join(values) + '\n'
+
+
+    return out
