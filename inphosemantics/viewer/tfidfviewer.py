1df3e9a401e5ea983093cc3e8429d64b5501f7e8
Robert Rose <rrose1@indiana.edu>
2012-07-25 14:05:34 -0400

    TfIdfViewer is identical to TfViewer for now

--- inphosemantics/viewer/tfidfviewer.py
+++ inphosemantics/viewer/tfidfviewer.py
@@ -1,24 +1,7 @@
-import inphosemantics.viewer as vw
+from inphosemantics.viewer import tfviewer
 
 
-class TfIdfViewer(vw.Viewer):
+class TfIdfViewer(tfviewer.TfViewer):
 
+    pass
 
-    def similar_terms(self, term, filter_nan=False):
-
-        return vw.similar_terms(self, term, filter_nan)
-
-
-    def similar_documents(self, document, filter_nan=False):
-
-        return vw.similar_documents(self, document, filter_nan)
-
-
-    def simmat_terms(self, term_list):
-
-        return vw.simmat_terms(self, term_list)
-
-
-    def simmat_documents(self, document_list):
-
-        return vw.simmat_documents(self, document_list)
