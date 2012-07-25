2f031f55b099631a0c6649afd44d442be3de66b4
Samuel Waggoner <srwaggon@indiana.edu>
2012-07-25 13:25:57 -0400

    Beginnings of implicit schema building. CouchDB class for Corpora and Masked Corpora.

--- inphosemantics/tests/tests_corpus.py
+++ inphosemantics/tests/tests_corpus.py
@@ -1,13 +1,36 @@
-from inphosemantics.corpus import *
+from inphosemantics import corpus
 from inphosemantics.corpus import tokenizer
 
+from couchdb import * # experiment database
+from couchdb.mapping import TextField, IntegerField, DateTimeField
 
 
-plain_path = 'inphosemantics/tests/data/iep/selected/corpus/plain'
+from datetime import datetime
 
-stoplist_path = 'inphosemantics/tests/data/stoplists/stoplist-nltk-english.txt'
+
+
+## iep_test_plain_path
+#plain_path = 'inphosemantics/tests/data/iep/selected/corpus/plain'
+
+#stoplist_path = 'inphosemantics/tests/data/stoplists/stoplist-nltk-english.txt'
 
 corpus_filename = 'inphosemantics/tests/data/iep/selected/corpus/iep-selected-nltk.npz'
+
+
+
+## experiment boilerplate
+couchServer = Server()
+db = couchServer['inphosemantics']
+
+
+
+## TODO: Set up this data to be fetched
+## Fetch some test data
+plain_path = db['iep_test']['plain_path']
+
+stoplist_file = db['iep_test']['stoplists']['nltk']
+
+#corpus_filename = db['iep_test'][]
 
 
 
@@ -36,34 +59,57 @@ def load_test_stoplist():
 
 
 
+class Metadata(Document):
+    added = DateTimeField(default=datetime.now)
+    type  = TextField()
+    
+    
+class CorpusMeta(Metadata):
+    plain_path = TextField()
+    raw_path   = TextField()
+    stoplists  = ListField()
+
+    
+class MaskedCorpusMeta(MetaData):
+    Corpus = TextField() # contains the _id of the corpus being masked
+    masking_functions = ListField()
+    output_filename = TextField(default=corpus_filename)
+
+
 
 # Corpus tests
 
 def test_corpus():
 
-    terms, tok_names, tok_data = tokenize_test_corpus()
+    ## run the experiment
+    try:
+        terms, tok_names, tok_data = tokenize_test_corpus()
+        c = corpus.Corpus(terms,
+                   tok_names=tok_names,
+                   tok_data=tok_data)
 
-    c = Corpus(terms,
-               tok_names=tok_names,
-               tok_data=tok_data)
+        print 'First article:\n',\
+              c.view_tokens('articles', True)[1]
+        print '\nFirst five paragraphs:\n',\
+              c.view_tokens('paragraphs', True)[:5]
+        print '\nFirst ten sentences:\n',\
+              c.view_tokens('sentences', True)[:10]
+        
+        print '\nLast article:\n',\
+              c.view_tokens('articles', True)[-1]
+        print '\nLast five paragraphs:\n',\
+              c.view_tokens('paragraphs', True)[-5:]
+        print '\nLast ten sentences:\n',\
+              c.view_tokens('sentences', True)[-10:]
+        
+        print '\nSource of second article:',\
+              c.view_metadata('articles')[2]
 
-    print 'First article:\n',\
-          c.view_tokens('articles', True)[1]
-    print '\nFirst five paragraphs:\n',\
-          c.view_tokens('paragraphs', True)[:5]
-    print '\nFirst ten sentences:\n',\
-          c.view_tokens('sentences', True)[:10]
+    ## Catch any problems
+    except:
+        pass
 
-    print '\nLast article:\n',\
-          c.view_tokens('articles', True)[-1]
-    print '\nLast five paragraphs:\n',\
-          c.view_tokens('paragraphs', True)[-5:]
-    print '\nLast ten sentences:\n',\
-          c.view_tokens('sentences', True)[-10:]
-
-    print '\nSource of second article:',\
-          c.view_metadata('articles')[2]
-
+    ## Return the result
     return c
 
 
@@ -75,25 +121,24 @@ def test_corpus():
 
 
 def test_masked_corpus_1():
-
     text = ['I', 'came', 'I', 'saw', 'I', 'conquered']
     tok_names = ['sentences']
     tok_data = [[(2, 'Veni'), (4, 'Vidi'), (6, 'Vici')]]
     masked_terms = ['I', 'came']
-
-    c = MaskedCorpus(text,
-                     tok_names=tok_names,
-                     tok_data=tok_data,
-                     masked_terms=masked_terms)
-
+    
+    c = corpus.MaskedCorpus(text,
+                            tok_names=tok_names,
+                            tok_data=tok_data,
+                            masked_terms=masked_terms)
+    
     from tempfile import TemporaryFile
     tmp = TemporaryFile()
     c.save(tmp)
-
+    
     tmp.seek(0)
-
-    c_in = MaskedCorpus.load(tmp)
-
+    
+    c_in = corpus.MaskedCorpus.load(tmp)
+    
     print 'corpus:', c_in.corpus
     print 'data:', c_in.corpus.data
     print 'mask:', c_in.corpus.mask
@@ -104,6 +149,7 @@ def test_masked_corpus_1():
     print 'masked terms:', c_in.masked_terms
     print 'compressed_corpus:', c_in.compressed_corpus()
     
+    # Return the result
     return c_in
 
 
@@ -115,15 +161,15 @@ def test_masked_corpus_2():
 
     terms, tok_names, tok_data = tokenize_test_corpus()
 
-    c = MaskedCorpus(terms,
+    c = corpus.MaskedCorpus(terms,
                      tok_names=tok_names,
                      tok_data=tok_data)
 
     stoplist = load_test_stoplist()
 
-    mask_sing_occ(c)
+    corpus.mask_sing_occ(c)
 
-    mask_from_stoplist(c, stoplist)
+    corpus.mask_from_stoplist(c, stoplist)
 
     print 'First article:\n',\
           c.view_tokens('articles', True)[1]
@@ -167,22 +213,16 @@ def test_compressed():
 
 
 
-
-
 def test_masked_corpus_save():
-
     terms, tok_names, tok_data = tokenize_test_corpus()
-
-    c = MaskedCorpus(terms,
-                     tok_names=tok_names,
-                     tok_data=tok_data)
-
+        
+    c = corpus.MaskedCorpus(terms,
+                            tok_names=tok_names,
+                            tok_data=tok_data)
+    
     stoplist = load_test_stoplist()
-
-    mask_from_stoplist(c, stoplist)
-
+    
+    corpus.mask_from_stoplist(c, stoplist)
+    
     c.save(corpus_filename)
-
-
-
-
+    
