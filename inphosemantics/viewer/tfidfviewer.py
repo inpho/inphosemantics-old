1977ac164128cb7d9a2195e2c92641a9f8154a11
Robert Rose <rrose1@indiana.edu>
2012-08-26 01:14:50 -0400

    TfIdfViewer only exposes similarity functions.

--- inphosemantics/viewer/tfidfviewer.py
+++ inphosemantics/viewer/tfidfviewer.py
@@ -1,7 +1,26 @@
-from inphosemantics.viewer import tfviewer
+import inphosemantics.viewer as vw
 
 
-class TfIdfViewer(tfviewer.TfViewer):
 
-    pass
+class TfIdfViewer(vw.Viewer):
+    """
+    """
+    def similar_terms(self, term, filter_nan=False):
+
+        return vw.similar_terms(self, term, filter_nan)
+
+
+    def similar_documents(self, document, filter_nan=False):
+
+        return vw.similar_documents(self, document, filter_nan)
+
+
+    def simmat_terms(self, term_list):
+
+        return vw.simmat_terms(self, term_list)
+
+
+    def simmat_documents(self, document_list):
+
+        return vw.simmat_documents(self, document_list)
 
